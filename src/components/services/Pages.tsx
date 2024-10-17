261903a47beb3eda6d6601e5d686fe77d2fba95c
Banshita-Bansal <app5.nessco@gmail.com>
2024-10-17 15:57:18 +0530

    Services Page

---
+++ src/components/services/Pages.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page1 from "@/components/services/Header";
+
+const Pages = () => {
+  return (
+    <>
+      <Page1 />
+    </>
+  );
+};
+
+export default Pages;
