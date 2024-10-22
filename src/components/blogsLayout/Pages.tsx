5da4372958446aa743f3a8070829d6dfe8b268ae
Banshita-Bansal <app5.nessco@gmail.com>
2024-10-22 16:31:16 +0530

    Blogs Layout Page

---
+++ src/components/blogsLayout/Pages.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Page1 from "./Header";
+
+const Pages = () => {
+  return (
+    <>
+      <div className="lg:p-4 p-2">
+        <Page1 />
+      </div>
+    </>
+  );
+};
+
+export default Pages;
